4ab4db05b3f14162c4a413e1054b417e4e58f124
Guilherme Pravato <147657600+guilhermepravato@users.noreply.github.com>
2025-03-06 13:36:45 -0300

    dfgnm

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+
+
+    botaoDeAcessibilidade.addEventListener('click', function (){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+       })
+       const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+       const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+
 
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
@@ -18,7 +27,3 @@ document.addEventListener('DOMContentLoaded', function(){
     });
 
 });
-botaoDeAcessibilidade.addEventListener('click', function (){
-    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
-   })
